778033b3a39335a283b3c93930c53b5d7cf26440
GitTechno <55440807+GitTechno@users.noreply.github.com>
2021-05-08 23:29:47 +0530

    Add files via upload

---
+++ Assingment1/ForLoop.java
@@ -0,0 +1,36 @@
+class ForLoop
+{
+  public static void main(String[] args)
+  {
+     
+	/* for(int i=0;i<=10;i++)
+	 {
+	   System.out.println(i);
+	 }  */
+	 
+	 
+	/* for(int i=10;i>=0;i--)
+	 {
+      System.out.println(i);
+     }  */	  
+	 
+	 //Sum of first 15 natural numbers
+	 
+	 int n,sum=0;
+	 n=15;
+	 
+	 for(int i=0;i<=15;i++)
+	 {
+		 sum=sum+i;
+		 
+		 System.out.println("in Loop"+sum);
+		 
+		 
+	 }
+      
+      System.out.println("Sum of 15 natural number is ::"+sum);	  
+	 
+	 
+  }
+	 
+}
---
+++ Assingment1/Hello.java
@@ -0,0 +1,13 @@
+
+class Hello
+{
+
+   public static void main(String args[]){
+      
+	  String s1=args[0];
+	  String s2=args[1];
+      System.out.println("Hello :"+s1);
+	  System.out.println("Welcome to CDAC :"+s2);
+   }
+
+}
